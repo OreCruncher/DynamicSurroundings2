f3535264914624242f6476aff07fde3c7d3121d6
OreCruncher <tedjkaiser@gmail.com>
2020-03-09 07:08:46 -0700

    Dedicated server crash

--- src/main/java/org/orecruncher/sndctrl/SoundControl.java
+++ src/main/java/org/orecruncher/sndctrl/SoundControl.java
@@ -18,6 +18,7 @@
 
 package org.orecruncher.sndctrl;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -27,6 +28,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.orecruncher.lib.GameUtils;
 import org.orecruncher.lib.effects.EntityEffectHandler;
 import org.orecruncher.lib.fml.ConfigUtils;
@@ -64,16 +66,17 @@ public final class SoundControl {
     public static final Path CONFIG_PATH = ConfigUtils.getConfigPath(MOD_ID);
 
     public SoundControl() {
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            // Various event bus registrations
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupComplete);
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+            MinecraftForge.EVENT_BUS.register(this);
 
-        // Various event bus registrations
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupComplete);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        MinecraftForge.EVENT_BUS.register(this);
-
-        // Initialize our configuration
-        Config.setup();
+            // Initialize our configuration
+            Config.setup();
+        }
     }
 
     private void commonSetup(@Nonnull final FMLCommonSetupEvent event) {

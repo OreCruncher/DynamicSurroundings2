ceaac5cde207ca7009f6b484ec5b09925eb547b7
OreCruncher <tedjkaiser@gmail.com>
2021-01-05 12:50:18 -0800

    Templates for armor accents and footstep sounds

--- src/main/java/org/orecruncher/sndctrl/library/Primitives.java
+++ src/main/java/org/orecruncher/sndctrl/library/Primitives.java
@@ -27,15 +27,19 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.orecruncher.lib.SoundTypeUtils;
 import org.orecruncher.lib.Utilities;
+import org.orecruncher.lib.resource.ResourceUtils;
 import org.orecruncher.mobeffects.MobEffects;
 import org.orecruncher.mobeffects.config.Config;
 import org.orecruncher.mobeffects.library.Constants;
+import org.orecruncher.sndctrl.SoundControl;
 import org.orecruncher.sndctrl.api.acoustics.IAcoustic;
 import org.orecruncher.sndctrl.api.sound.ISoundCategory;
+import org.orecruncher.sndctrl.audio.acoustic.AcousticCompiler;
 import org.orecruncher.sndctrl.audio.acoustic.NullAcoustic;
 import org.orecruncher.sndctrl.audio.acoustic.SimpleAcoustic;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 import java.util.Optional;
 
 @OnlyIn(Dist.CLIENT)
@@ -48,6 +52,9 @@ public class Primitives {
     private static final String FOOTSTEP_PREFIX = "primitive/block/step/";
     private static final String VANILLA_FOOTSTEP_PREFIX = "primitive/block/step/vanilla/";
     private static final String SOUND_PREFIX = "primitive/sound/";
+
+    private static final ResourceLocation ARMOR_TEMPLATE = new ResourceLocation(SoundControl.MOD_ID, "templates/primitive_armor_accent.json");
+    private static final ResourceLocation FOOTSTEP_TEMPLATE = new ResourceLocation(SoundControl.MOD_ID, "templates/primitive_block_step.json");
 
     @Nonnull
     public static ResourceLocation createArmorToolbarResource(@Nonnull final IArmorMaterial material) {
@@ -110,6 +117,12 @@ public class Primitives {
     @Nonnull
     public static IAcoustic getArmorAccentAcoustic(@Nonnull final IArmorMaterial material) {
         final ResourceLocation loc = createArmorAccentResource(material);
+        IAcoustic acoustic = AcousticLibrary.resolve(loc);
+        if (acoustic == NullAcoustic.INSTANCE) {
+            final ResourceLocation soundName = material.getSoundEvent().getName();
+            acoustic = generateAcousticFromTemplate(ARMOR_TEMPLATE, loc, soundName);
+            AcousticLibrary.addAcoustic(loc, acoustic);
+        }
         return footstepAcousticResolver(loc, material.getSoundEvent());
     }
 
@@ -121,13 +134,13 @@ public class Primitives {
     @Nonnull
     public static IAcoustic getVanillaFootstepAcoustic(@Nonnull final SoundType soundType) {
         final ResourceLocation loc = createVanillaFootstepResource(soundType);
-        return footstepAcousticResolver(loc, soundType);
+        return footstepAcousticResolver(loc, soundType, true);
     }
 
     @Nonnull
     public static IAcoustic getFootstepAcoustic(@Nonnull final SoundType soundType) {
         final ResourceLocation loc = createFootstepResource(soundType);
-        return footstepAcousticResolver(loc, soundType);
+        return footstepAcousticResolver(loc, soundType, false);
     }
 
     @Nonnull
@@ -143,16 +156,36 @@ public class Primitives {
     }
 
     @Nonnull
-    private static IAcoustic footstepAcousticResolver(@Nonnull final ResourceLocation loc, @Nonnull final SoundType soundType) {
+    private static IAcoustic footstepAcousticResolver(@Nonnull final ResourceLocation loc, @Nonnull final SoundType soundType, final boolean isVanilla) {
         IAcoustic acoustic = AcousticLibrary.resolve(loc);
         if (acoustic == NullAcoustic.INSTANCE) {
-            final SimpleAcoustic simple = new SimpleAcoustic(soundType.getStepSound(), Constants.FOOTSTEPS);
-            // Minecraft scales footstep sound volume to 15%
-            simple.getFactory().setVolume(soundType.getVolume() * (MINECRAFT_VOLUME_SCALE / (Config.FOOTSTEP_VOLUME_DEFAULT / 100F)));
-            simple.getFactory().setPitch(soundType.getPitch());
-            AcousticLibrary.addAcoustic(loc, simple);
-            acoustic = simple;
+            if (isVanilla) {
+                final SimpleAcoustic simple = new SimpleAcoustic(soundType.getStepSound(), Constants.FOOTSTEPS);
+                // Minecraft scales footstep sound volume to 15%
+                simple.getFactory().setVolume(soundType.getVolume() * (MINECRAFT_VOLUME_SCALE / (Config.FOOTSTEP_VOLUME_DEFAULT / 100F)));
+                simple.getFactory().setPitch(soundType.getPitch());
+                AcousticLibrary.addAcoustic(loc, simple);
+                acoustic = simple;
+            } else {
+                final ResourceLocation soundResource = soundType.getStepSound().getRegistryName();
+                if (soundResource != null)
+                    acoustic = generateAcousticFromTemplate(FOOTSTEP_TEMPLATE, loc, soundResource);
+                else
+                    acoustic = NullAcoustic.INSTANCE;
+            }
         }
         return acoustic;
+    }
+
+    private static IAcoustic generateAcousticFromTemplate(@Nonnull final ResourceLocation template, @Nonnull final ResourceLocation name, @Nonnull final ResourceLocation acousticName) {
+        String jsonResource = ResourceUtils.readResource("", template);
+        if (jsonResource != null) {
+            jsonResource = jsonResource
+                    .replaceAll("<NAME>", name.toString())
+                    .replaceAll("<ACOUSTIC>", acousticName.toString());
+            final List<IAcoustic> acoustic = new AcousticCompiler(MobEffects.MOD_ID).compile(jsonResource);
+            return acoustic.get(0);
+        }
+        return NullAcoustic.INSTANCE;
     }
 }
